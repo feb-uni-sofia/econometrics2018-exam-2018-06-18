f57fac46e468cb9a3d28d9bccdb83363aaa6b454
Boyko Amarov <boyko.amarov@gmail.com>
2018-06-18 10:46:14 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,23 @@
+## Exam 1 / Problem 1
 
+library(dplyr)
+library(ggplot2)
+
+internet <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/internet.csv')
+
+## a)
+
+## b)
+
+## c)
+
+## d)
+
+## e)
+
+## f) 
+## No R code needed
+
+## g)
+
+## h)
