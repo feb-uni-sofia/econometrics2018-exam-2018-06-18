4aa03315a9d3bfd9839a9121509c4dc297f979a6
Boyko Amarov <boyko.amarov@gmail.com>
2018-06-18 10:46:54 +0300

    Update Problem_2.R

--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,17 @@
 
+## Exam 1 / Problem 2
+houses <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houses.csv')
+str(houses)
+
+## a)
+
+## b)
+
+## c)
+
+## d)
+## No R code required.
+
+## e)
+
+## f)
