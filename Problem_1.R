2cdc6679fd9d1c04e7c3e7b9b8d49cb1cbd28da5
Boyko Amarov <boyko.amarov@gmail.com>
2019-06-06 17:09:45 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,17 +1,25 @@
 ## Exam 1 / Problem 1
 
-library(dplyr)
-library(ggplot2)
-
 internet <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/internet.csv')
 
 ## a)
 
+mean(internet$penetration)
+
 ## b)
 
+internet <- within(internet, {
+  lowFacebookUse <- facebook < 32
+})
+
 ## c)
+table(internet$lowFacebookUse)
+sum(internet$lowFacebookUse)
 
 ## d)
+
+fit <- lm(penetration ~ lowFacebookUse, data = internet)
+summary(fit)
 
 ## e)
 
@@ -19,5 +27,6 @@ internet <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/dat
 ## No R code needed
 
 ## g)
+qt(0.025, df = 31 - 2)
 
 ## h)
